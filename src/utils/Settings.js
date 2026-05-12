372fbe209ad022b842bc011ef89504d44cc72300
HoMaX <nicto.mist1@gmail.com>
2026-05-12 12:43:18 +0300

    1

---
+++ src/utils/Settings.js
@@ -0,0 +1,20 @@
+import * as THREE from 'three';
+
+export class Settings{
+    constructor(scene){
+        this.scene = scene;
+    }
+
+    createAllHelpers(){
+        _createGridHelper();
+    }
+    _createMesh
+    _createGridHelper(){
+
+    }
+    _createCubeMesh(){
+        
+    }
+
+
+}
